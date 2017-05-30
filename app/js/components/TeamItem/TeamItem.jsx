8f822496039b90df255314c9abc5540567962bed
jasonreed7 <jreed0719@gmail.com>
2017-05-30 15:39:45 -0400

    Work on team page an contact page

--- app/js/components/TeamItem/TeamItem.jsx
+++ app/js/components/TeamItem/TeamItem.jsx
@@ -8,6 +8,11 @@ export default class TeamItem extends React.Component {
 			<div className='team-item text-center text-sm'>
 				<div className='team-picture-container text-center'>
 					<img className='team-picture' src={ this.props.person.picture } />
+					<div className='team-text-overlay'>
+						<div className='team-name'>{ this.props.person.name }</div>
+						<div className='team-title'>{ this.props.person.title }</div>
+						<div className='team-description'>{ this.props.person.description }</div>
+					</div>
 				</div>
 				<div className='team-text'>
 					<div className='team-name'>{ this.props.person.name }</div>
